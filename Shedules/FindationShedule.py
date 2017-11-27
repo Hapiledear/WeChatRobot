9e89b5eb682d90782a02f40e7c80e2e09b686cb8
yanghuang <yang_huang@suixingpay.com>
2017-11-27 10:43:37 +0800

    基金 定时发送消息

--- Shedules/FindationShedule.py
+++ Shedules/FindationShedule.py
@@ -28,19 +28,13 @@ _schedual = ""
 
 
 def start_apshedule():
-    # targets = itchat.search_chatrooms(name='奔小康')
-    userNames = ['奔小康']
-    # if targets:
-    #     for target in targets:
-    #         userNames.append(target.UserName)
-    # else:
-    #     LOGGER.warning("未找到相关群聊，请检查是否变更了群名称")
+    nickNames = ['奔小康']
 
     sched = BackgroundScheduler(daemonic=False)
     sched._logger = LOGGER
-    sched.add_job(func=scrapAndSenFinMsg, args=[userNames], replace_existing=True, misfire_grace_time=3,
+    sched.add_job(func=scrapAndSenFinMsg, args=[nickNames], replace_existing=True, misfire_grace_time=3,
                   trigger='cron', name='基金查询_预估值', id='scrap_fin', minute='0,15,30,45', hour='9-14', day_of_week='0-4')
-    sched.add_job(func=scrapAndSenFinAccMsg, args=[userNames], replace_existing=True, misfire_grace_time=3,
+    sched.add_job(func=scrapAndSenFinAccMsg, args=[nickNames], replace_existing=True, misfire_grace_time=3,
                   trigger='cron', name='基金查询_实际值', id='scrap_fin_acc', minute='1', hour='22', day_of_week='0-4')
     sched.start()
     global _schedual
@@ -51,19 +45,41 @@ def stop_apshedule():
     _schedual.shutdown()
 
 
-def scrapAndSenFinMsg(userNames):
+def scrapAndSenFinMsg(nickNames):
     LOGGER.info("开始执行基金查询任务")
-    fund = Fundation(userNames)
+    fund = Fundation(nickNames)
     resMsg = fund.startScrapy(type=1)
     LOGGER.debug(resMsg)
+
+    targets = []
+    if nickNames:
+        for nickName in nickNames:
+            targets.append(itchat.search_chatrooms(name=nickName))
+    userNames = []
+    if targets:
+        for target in targets:
+            userNames.append(target.UserName)
+    else:
+        LOGGER.warning("未找到相关群聊，请检查是否变更了群名称")
     for userName in userNames:
         itchat.send_msg(resMsg, toUserName=userName)
 
 
-def scrapAndSenFinAccMsg(userNames):
+def scrapAndSenFinAccMsg(nickNames):
     LOGGER.info("开始执行基金查询任务")
-    fund = Fundation(userNames)
+    fund = Fundation(nickNames)
     resMsg = fund.startScrapy(type=2)
     LOGGER.debug(resMsg)
+
+    targets = []
+    if nickNames:
+        for nickName in nickNames:
+            targets.append(itchat.search_chatrooms(name=nickName))
+    userNames = []
+    if targets:
+        for target in targets:
+            userNames.append(target.UserName)
+    else:
+        LOGGER.warning("未找到相关群聊，请检查是否变更了群名称")
     for userName in userNames:
         itchat.send_msg(resMsg, toUserName=userName)
